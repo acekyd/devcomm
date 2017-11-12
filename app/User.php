ce046e51b1d0040dc42442bc43cfbd6e7a4b7aa9
Pluto Slime <vikie95@gmail.com>
2017-11-12 13:50:11 +0100

    Added NotificationSetting model and migration, and defined one to one relationship between NotificationSetting and User

--- app/User.php
+++ app/User.php
@@ -27,5 +27,9 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
-    ];
+	];
+
+	public function notificationSetting() {
+		return $this->hasOne('App\NotificationSetting');
+	}
 }
