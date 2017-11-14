6fdc5e792fcab011d3645177173a53eab8adad96
Abati Adewale <acekyd01@gmail.com>
2017-11-14 02:45:32 +0100

    Laravel Auth + React Pages

---
+++ resources/assets/js/components/Master.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Router, Route, Link } from 'react-router';
+
+class Master extends Component {
+  render(){
+    return (
+      <div className="container">
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">AppDividend</a>
+            </div>
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Home</a></li>
+              <li><a href="#">Page 1</a></li>
+              <li><a href="#">Page 2</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+          </div>
+      </nav>
+          <div>
+              {this.props.children}
+          </div>
+      </div>
+    )
+  }
+}
+export default Master;
