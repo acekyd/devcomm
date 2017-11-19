9e89343382671680196d34329bf04875730eefbd
Abati Adewale <acekyd01@gmail.com>
2017-11-19 13:00:04 +0100

    Configure dingo API

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,27 @@
 <?php
 
-// Route::get('swcmc', 'UserController@statesWithCommunityMemberCount');
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+	$api->get('/test', function() {
+		return ['Ace baba!!'];
+	});
+
+	$api->post('/login', 'App\Http\Controllers\AuthController@login');
+	$api->post('/signup', 'App\Http\Controllers\AuthController@signup');
+	$api->post('/recovery', 'App\Http\Controllers\AuthController@recovery');
+	$api->post('/reset', 'App\Http\Controllers\ResetPasswordController@reset');
+});
