11eb612bd8c742d15e5ad5ac874cf2fa47764d4f
Goodness Toluwanimi Kayode <gtkbrain@gmail.com>
2017-11-22 09:20:13 +0100

    make custom 404 awesome

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,38 @@
-custom 404 error
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occured, Requested page not found!
+                </div>
+                <div class="error-actions">
+                    <a href="{{ route('/') }}" class="btn btn-primary btn-lg"><span class="fa fa-home"></span>
+                        Take Me Home 
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style type="text/stylesheet">
+.error-template {
+          padding: 40px 15px;
+          text-align: center;
+ }
+.error-actions {
+          margin-top:15px;
+          margin-bottom:15px;
+}
+.error-actions .btn {
+            margin-right:10px; 
+ }
+</style> 
+@endsection
