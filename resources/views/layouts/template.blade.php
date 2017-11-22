104c79708d9de6edd5f1048bcde06d5b8e5d32ce
Goodness Toluwanimi Kayode <gtkbrain@gmail.com>
2017-11-22 09:04:29 +0100

    fix promote route with route helper

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -17,7 +17,7 @@
                 <div class='menu'>
                     <div class='brand'>DevComm</div>
                     <div class='links'>
-                        <a href="/promote">Promote</a>
+                        <a href="{{ route('promote') }}">Promote</a>
                         <ul class="nav navbar-nav navbar-right">
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
