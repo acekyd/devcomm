8aaec22314d5793e50d5733b87c1b8358dbe0635
Abati Adewale <acekyd01@gmail.com>
2017-12-12 23:26:05 +0100

    Added Google analytics #29

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,6 +10,7 @@ require('./bootstrap');
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import ReactGA from 'react-ga';
 
 /**
  * Next, we will create a fresh React component instance and attach it to
@@ -19,5 +20,7 @@ import ReactDom from 'react-dom';
 
 
 import { Routes } from './config';
+ReactGA.initialize('UA-111097304-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDom.render(<Routes />, document.getElementById('app'));
