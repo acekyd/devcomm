ff3d917cfaed30db46b89227e990d62f6324e16e
Adewale Abati <acekyd01@gmail.com>
2018-06-08 10:54:40 +0100

    Implement 404 page and route switching

--- resources/assets/js/config/routes.js
+++ resources/assets/js/config/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
 	BrowserRouter as Router,
-	Route, Link
+	Route, Link, Switch
 } from 'react-router-dom';
 import * as Scenes from '../scenes';
 
@@ -9,15 +9,20 @@ export default function Routes(props) {
 	return (
 		<Router>
 			<div>
-				<Route exact path='/' component={Scenes.Landing}/>
-				<Route path='/login' component={Scenes.authScenes.Login}/>
-				<Route path='/logout' component={Scenes.authScenes.Logout}/>
-				<Route path='/register' component={Scenes.authScenes.Register}/>
-				<Route path='/home' component={Scenes.Home}/>
-				<Route path='/profile/edit' component={Scenes.Config}/>
-				<Route path='/promote' component={Scenes.Promote}/>
-				<Route path='/u/:page' component={Scenes.Profile}/>
-				<Route path='/location/:state' component={Scenes.Home}/>
+				<Switch>
+					<Route exact path='/' component={Scenes.Landing}/>
+					<Route path='/login' component={Scenes.authScenes.Login}/>
+					<Route path='/logout' component={Scenes.authScenes.Logout}/>
+					<Route path='/register' component={Scenes.authScenes.Register}/>
+					<Route path='/home' component={Scenes.Home}/>
+					<Route path='/profile/edit' component={Scenes.Config}/>
+					<Route path='/promote' component={Scenes.Promote}/>
+					<Route path='/u/:page' component={Scenes.Profile}/>
+					<Route path='/location/:state' component={Scenes.Home}/>
+
+					{/* add 404 page */}
+					<Route path="*" component={Scenes.NotFound} />
+				</Switch>
 			</div>
 		</Router>
 	);
